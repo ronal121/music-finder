9fd2ebf4891e5284a87688b645cff015a344177d
ronal121 <142820732+ronal121@users.noreply.github.com>
2026-08-19 18:42:44 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.kafshar.musicfinder"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
+        versionCode = 5
         versionName = "1.0"
     }
 
@@ -26,10 +26,20 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
 
-    implementation("androidx.media3:media3-exoplayer:1.5.1")
-    implementation("androidx.media3:media3-ui:1.5.1")
+    implementation("androidx.core:core-ktx:1.15.0")
+
+    implementation("androidx.appcompat:appcompat:1.7.0")
+
+    implementation(
+        "com.google.android.material:material:1.12.0"
+    )
+
+    implementation(
+        "androidx.media3:media3-exoplayer:1.5.1"
+    )
+
+    implementation(
+        "androidx.media3:media3-ui:1.5.1"
+    )
 }
