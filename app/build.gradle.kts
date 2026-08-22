76563c448e1f18e05ff18d18127b271e90f6fa55
ronal121 <142820732+ronal121@users.noreply.github.com>
2026-08-22 23:49:48 +0330

    build: prepare version 1.2 and add unit-test dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,74 +5,39 @@ plugins {
 
 android {
     namespace = "com.kafshar.musicfinder"
-
     compileSdk = 35
 
     defaultConfig {
         applicationId = "com.kafshar.musicfinder"
-
         minSdk = 24
         targetSdk = 35
-
-        versionCode = 11
-        versionName = "1.1"
+        versionCode = 12
+        versionName = "1.2"
     }
 
     compileOptions {
-        sourceCompatibility =
-            JavaVersion.VERSION_17
-
-        targetCompatibility =
-            JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
-    }
+    kotlinOptions { jvmTarget = "17" }
 
     buildTypes {
-
         release {
             isMinifyEnabled = false
             isShrinkResources = false
-
-            proguardFiles(
-                getDefaultProguardFile(
-                    "proguard-android-optimize.txt"
-                ),
-                "proguard-rules.pro"
-            )
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
-
-        debug {
-            isMinifyEnabled = false
-        }
+        debug { isMinifyEnabled = false }
     }
 }
 
 dependencies {
-
-    implementation(
-        "androidx.core:core-ktx:1.15.0"
-    )
-
-    implementation(
-        "androidx.appcompat:appcompat:1.7.0"
-    )
-
-    implementation(
-        "com.google.android.material:material:1.12.0"
-    )
-
-    implementation(
-        "androidx.media3:media3-exoplayer:1.5.1"
-    )
-
-    implementation(
-        "androidx.media3:media3-session:1.5.1"
-    )
-
-    implementation(
-        "androidx.media3:media3-ui:1.5.1"
-    )
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.media3:media3-exoplayer:1.5.1")
+    implementation("androidx.media3:media3-session:1.5.1")
+    implementation("androidx.media3:media3-ui:1.5.1")
+    testImplementation("junit:junit:4.13.2")
 }
