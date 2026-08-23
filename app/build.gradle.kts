bbd7d5d009e17e936469ecc1152e78d34c5e4c50
ronal121 <142820732+ronal121@users.noreply.github.com>
2026-08-23 06:26:23 +0330

    Bump app version for site lyrics support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.kafshar.musicfinder"
         minSdk = 24
         targetSdk = 35
-        versionCode = 12
-        versionName = "1.2"
+        versionCode = 13
+        versionName = "1.3"
     }
 
     compileOptions {
