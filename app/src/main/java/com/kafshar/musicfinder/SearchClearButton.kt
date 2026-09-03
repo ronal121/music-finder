2aa60a8ba6a25beb4556955fdd7121e77713cbd0
ronal121 <142820732+ronal121@users.noreply.github.com>
2026-09-03 11:27:08 +0330

    Fix search clear button compilation

--- app/src/main/java/com/kafshar/musicfinder/SearchClearButton.kt
+++ app/src/main/java/com/kafshar/musicfinder/SearchClearButton.kt
@@ -2,12 +2,13 @@ package com.kafshar.musicfinder
 
 import android.content.Context
 import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
 
 class SearchClearButton @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = android.R.attr.textViewStyle
-) : HarmonizedButton(context, attrs, defStyleAttr) {
+) : AppCompatTextView(context, attrs, defStyleAttr) {
 
     init {
         setOnClickListener {
